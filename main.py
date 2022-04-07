a395fff1958c63fa38010a8e5bc024fa6d3c6062
Medokins <nikodemszwast@gmail.com>
2022-04-07 20:59:39 +0200

    add first steps into getting frequency

---
+++ main.py
@@ -0,0 +1,39 @@
+import pyaudio
+import wave
+import numpy as np
+
+chunk = 2048
+
+wf = wave.open('wav_music/Meltt - Only In Your Eyes.wav', 'rb')
+swidth = wf.getsampwidth()
+RATE = wf.getframerate()
+window = np.blackman(chunk)
+p = pyaudio.PyAudio()
+stream = p.open(format =
+                p.get_format_from_width(wf.getsampwidth()),
+                channels = wf.getnchannels(),
+                rate = RATE,
+                output = True)
+
+data = wf.readframes(chunk)
+
+while len(data) == chunk*swidth:
+    stream.write(data)
+    indata = np.array(wave.struct.unpack("%dh"%(len(data)/swidth),\
+                                         data))*window
+    fftData=abs(np.fft.rfft(indata))**2
+    which = fftData[1:].argmax() + 1
+    if which != len(fftData)-1:
+        y0,y1,y2 = np.log(fftData[which-1:which+2:])
+        x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
+        thefreq = (which+x1)*RATE/chunk
+        print("The freq is %f Hz.") % (thefreq)
+    else:
+        thefreq = which*RATE/chunk
+        print("The freq is %f Hz.") % (thefreq)
+    data = wf.readframes(chunk)
+if data:
+    stream.write(data)
+
+stream.close()
+p.terminate()
