70a1ed50a5310ca08d96922f65ca52d678889fd5
Medokins <nikodemszwast@gmail.com>
2022-04-09 21:30:17 +0200

    add first frequenct detection

--- main.py
+++ main.py
@@ -1,41 +1,31 @@
-import pyaudio
-import wave
+from aubio import source, pitch
 import numpy as np
 
-chunk = 2048
+win_s = 1010
+hop_s = 1000
 
-wf = wave.open('wav_music/Meltt - Only In Your Eyes.wav', 'rb')
-swidth = wf.getsampwidth()
-RATE = wf.getframerate()
-window = np.blackman(chunk)
-p = pyaudio.PyAudio()
-stream = p.open(format =
-                p.get_format_from_width(swidth),
-                channels = wf.getnchannels(),
-                rate = RATE,
-                output = True)
+samplerate = 48000
 
-data = wf.readframes(chunk)
+s = source("wav_music\Meltt - Only In Your Eyes.wav", samplerate, hop_s)
+samplerate = s.samplerate
 
-print(len(data))
-print(chunk*swidth)
+tolerance = 0.8
 
-while len(data) == chunk*swidth*2:
-    stream.write(data)
-    indata = np.array(wave.struct.unpack("%dh"%(len(data)/swidth), data*2))*window
-    fftData=abs(np.fft.rfft(indata))**2
-    which = fftData[1:].argmax() + 1
-    if which != len(fftData)-1:
-        y0,y1,y2 = np.log(fftData[which-1:which+2:])
-        x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
-        thefreq = (which+x1)*RATE/chunk
-        print("The freq is %f Hz.") % (thefreq)
-    else:
-        thefreq = which*RATE/chunk
-        print("The freq is %f Hz.") % (thefreq)
-    data = wf.readframes(chunk)
-if data:
-    stream.write(data)
+pitch_o = pitch("yin", win_s, hop_s, samplerate)
+pitch_o.set_unit("midi")
+pitch_o.set_tolerance(tolerance)
 
-stream.close()
-p.terminate()
+pitches = []
+confidences = []
+
+total_frames = 0
+while True:
+    samples, read = s()
+    pitch = pitch_o(samples)[0]
+    pitches += [pitch]
+    confidence = pitch_o.get_confidence()
+    confidences += [confidence]
+    total_frames += read
+    if read < hop_s: break
+
+print("Average frequency = " + str(np.array(pitches).mean()) + " hz")
