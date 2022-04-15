f998848daacbfc31fb8b32b0ab0fbebac7e1d591
Medokins <nikodemszwast@gmail.com>
2022-04-15 15:20:02 +0200

    add function that converts frequency to note

---
+++ frequencyConverter.py
@@ -0,0 +1,15 @@
+import math
+
+def frequency_to_note(freq):
+    notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+
+    # formula taken from https://en.wikipedia.org/wiki/Piano_key_frequencies
+    note_number = 12 * math.log2(freq / 440) + 49  
+    note_number = round(note_number)
+        
+    note = (note_number - 1 ) % len(notes)
+    note = notes[note]
+    
+    octave = (note_number + 8 ) // len(notes)
+    
+    return note, octave
