33988ec5a71b0fc49dd464ae743ff884dd21d8c2
Medokins <nikodemszwast@gmail.com>
2022-04-15 15:20:28 +0200

    update functionality

--- getVideo.py
+++ getVideo.py
@@ -1,10 +1,10 @@
-from random import vonmisesvariate
 import youtube_dl
 import subprocess
 
 def downloadVideo(video_url):
     video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
     filename = f"{video_info['title']}.mp3"
+    duration = video_info['duration']
     options={
         'format':'bestaudio/best',
         'keepvideo':False,
@@ -12,10 +12,7 @@ def downloadVideo(video_url):
     }
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
-
-    print(f"Download complete... {filename}")
+    return filename, duration
 
 def convertToWav(mp3_file_name, wav_output_name):
     subprocess.call(['ffmpeg', '-i', mp3_file_name + ".mp3", wav_output_name + ".wav"])
-
-convertToWav("music/Meltt - Only In Your Eyes", "wav_music/Meltt - Only In Your Eyes")
