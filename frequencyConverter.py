376bfe545e859fb75640f34a516724bc908f02b5
Medokins <nikodemszwast@gmail.com>
2022-04-16 19:28:45 +0200

    add safety check on log value

--- frequencyConverter.py
+++ frequencyConverter.py
@@ -4,6 +4,8 @@ def frequency_to_note(freq):
     notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
     # formula taken from https://en.wikipedia.org/wiki/Piano_key_frequencies
+    if freq == 0:
+        freq = 0.001
     note_number = 12 * math.log2(freq / 440) + 49  
     note_number = round(note_number)
         
