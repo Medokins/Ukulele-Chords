e88feabd22d642b5b086f443949ad7f758bc82b6
Medokins <nikodemszwast@gmail.com>
2022-04-25 21:29:34 +0200

    add framework for future function

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import numpy as np
 from getFrequency import freq
 from getVideo import downloadVideo, convertToWav
 from frequencyConverter import frequency_to_note
+from createImage import joinHorizontal, joinVertical, cutChords
 
-name = "sanah i Dawid Podsiadło „ostatnia nadzieja”" #if You want to get chords from an existing file
+name = "twenty one pilots House of Gold [OFFICIAL VIDEO]" #if You want to get chords from an existing file
 url = "https://www.youtube.com/watch?v=mDyxykpYeu8&ab_channel=FueledByRamen"
 frequency_table = []
 chords = []
@@ -25,4 +26,11 @@ for i in np.arange(0, int(length) - 1, step):
     frequency_table.append(frequency)
     chords.append(frequency_to_note(frequency))
 
-print(chords)
+#najpier pociac chords na 6-akrodowe wiersze i je wrzucic do tego co nizej
+
+chordsList = cutChords(chords, 6)
+
+for i in range(len(chordsList)):
+    joinHorizontal([i], i)
+
+joinVertical(chordsList, name)
