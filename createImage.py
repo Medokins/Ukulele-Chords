315cfde1a8c68580d62b5da7f433eee7a072e580
Medokins <nikodemszwast@gmail.com>
2022-04-25 21:29:58 +0200

    add framework for cutting chords function

--- createImage.py
+++ createImage.py
@@ -8,14 +8,14 @@ def joinHorizontal(chordsArray, row_number):
     imgs_comb = Image.fromarray(imgs_comb)
     imgs_comb.save(f"tempChords/{row_number}.png")    
 
-def joinVertical(number_of_rows, name):
+def joinVertical(number_of_rows, name, verbose=True):
     imgs = [Image.open(f"tempChords/{elem}.png") for elem in range(number_of_rows)]
     min_shape = sorted([(np.sum(i.size), i.size ) for i in imgs])[0][1]
     imgs_comb = np.vstack((np.asarray(i.resize(min_shape)) for i in imgs))
     imgs_comb = Image.fromarray( imgs_comb)
-    imgs_comb.save(f"outputChords/{name}.png")    
+    imgs_comb.save(f"outputChords/{name}.png")
+    if verbose:
+        imgs_comb.show()
 
-chords = ['A', 'A6', 'A7', 'A9']
-for i in range(4):
-    joinHorizontal(chords, i)
-joinVertical(4, "test")
+def cutChords(chords, number_in_row):
+    pass
